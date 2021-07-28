cc25d8d1aad060a0db42cbc8dc77a169c86f883c
nick belov <nickbelov2003@gmail.com>
2021-07-27 21:14:55 -0400

    Added a basic header

--- frontend/src/user/components/UsersList.js
+++ frontend/src/user/components/UsersList.js
@@ -1,25 +1,36 @@
-import React from 'react';
-import './UsersList.css';
-import UserItem from './UserItem';
+import React from "react";
+
+import "./UsersList.css";
+import UserItem from "./UserItem";
+import Card from "../../shared/components/UIElements/Card";
+
 const UsersList = (props) => {
-    //handling the case that we have no users yet
-    if (props.items.length === 0) {
+  //handling the case that we have no users yet
+  if (props.items.length === 0) {
+    return (
+      <div className="center">
+        <Card>
+          <h2>No users found</h2>
+        </Card>
+      </div>
+    );
+  }
+  //handling the case that we have users
+  return (
+    <ul className="users-list">
+      {props.items.map((user) => {
         return (
-            <div className="center">
-                <h2>No users found</h2>
-            </div>)
-    }
-    //handling the case that we have users
-    return <ul className="users-list">
-        {props.items.map(user => {
-            return <UserItem
-                key={user.id}
-                id={user.id}
-                image={user.image}
-                name={user.name}
-                placeCount={user.places} />;
-        })}
+          <UserItem
+            key={user.id}
+            id={user.id}
+            image={user.image}
+            name={user.name}
+            placeCount={user.places}
+          />
+        );
+      })}
     </ul>
+  );
 };
 
 export default UsersList;
