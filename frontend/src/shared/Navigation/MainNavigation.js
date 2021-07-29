d33e4f4e100af704777b7b9be7903bea95888df2
nick belov <nickbelov2003@gmail.com>
2021-07-29 15:32:14 -0400

    Animated sidedrawer and made it close when used

--- frontend/src/shared/Navigation/MainNavigation.js
+++ frontend/src/shared/Navigation/MainNavigation.js
@@ -7,39 +7,35 @@ import SideDrawer from "./SideDrawer";
 import Backdrop from "../components/UIElements/Backdrop";
 import "./MainNavigation.css";
 
-
 const MainNavigation = (props) => {
   const [drawerIsOpen, setDrawerIsOpen] = useState(false);
 
-  const openDrawer = () =>
-  {
+  const openDrawerHandler = () => {
     setDrawerIsOpen(true);
   };
-  const closeDrawer = () =>
-  {
+  const closeDrawerHandler = () => {
     setDrawerIsOpen(false);
   };
- 
 
   return (
     <React.Fragment>
-      {drawerIsOpen ? <Backdrop onClick={closeDrawer}/> : null}
-      {drawerIsOpen ? (
-        <SideDrawer>
-          <nav className="main-navigation__drawer-nav">
-            <NavLinks />
-          </nav>
-        </SideDrawer>
-      ) : null}
+      {drawerIsOpen ? <Backdrop onClick={closeDrawerHandler} /> : null}
+
+      <SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}>
+        <nav className="main-navigation__drawer-nav">
+          <NavLinks />
+        </nav>
+      </SideDrawer>
+
       <MainHeader>
-        <button className="main-navigation__menu-btn" onClick={openDrawer}>
+        <button className="main-navigation__menu-btn" onClick={openDrawerHandler}>
           <span />
           <span />
           <span />
         </button>
         <h1 className="main-navigation__title">
           <Link to="/">Nick's Dope Project</Link>
-        </h1> 
+        </h1>
         <nav className="main-navigation__header-nav">
           <NavLinks />
         </nav>
