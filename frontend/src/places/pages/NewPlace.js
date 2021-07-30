a88c38fc32d7f243adeea76bc9eda5cd0075e0e3
nick belov <nickbelov2003@gmail.com>
2021-07-30 01:23:00 -0400

    created the start of a custom form input

--- frontend/src/places/pages/NewPlace.js
+++ frontend/src/places/pages/NewPlace.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
+import Input from '../../shared/FormElements/Input';
+import './NewPlace.css'
+
 const NewPlace = () =>
 {
-    return <h2>New Place Works</h2>;
+    return <form className="place-form">
+        <Input type="text" label="Title" element="input"/>
+    </form>
 };
 export default NewPlace;
---
+++ frontend/src/shared/FormElements/Input.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import "./Input.css";
+
+//USE FORMIK LIBRARY IN THE FUTURE
+const Input = (props) => {
+  const element =
+    props.element === "input" ? (
+      <input id={props.id} type={props.type} placeholder={props.placeholder}/>
+    ) : (
+      <textarea id={props.id} rows={props.rows || 3}/>
+    );
+
+  return (
+    <div className={`form-control`}>
+      <label htmlFor={props.id}>{props.label}</label>
+      {element}
+    </div>
+  );
+};
+
+export default Input;
