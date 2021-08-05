fb30766d35ec6e8927e6be2a5a5fd1c34a586bad
Nick Belov <nickbelov@gmail.com>
2021-08-05 19:15:31 -0400

    git ignore added

--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -9,12 +9,6 @@ const Users = () => {
       image: "https://images.emojiterra.com/twitter/v13.0/512px/1f9a7.png",
       places: 3,
     },
-    {
-      id: "u2",
-      name: "Katy Belov",
-      image: "https://www.pinclipart.com/picdir/middle/405-4052086_mq-black-dolphin-silhouette-animal-common-bottlenose-dolphin.png",
-      places: 1,
-    },
   ];
 
   return <UsersList items={USERS} />;
