e927cec51535655bc17c92517178f471dd895a89
Nick Belov <nickbelov@gmail.com>
2021-08-08 20:22:41 -0400

    did some validating

--- backend/controllers/places-controllers.js
+++ backend/controllers/places-controllers.js
@@ -1,4 +1,5 @@
 const HttpError = require("../models/http-error");
+const {validationResult} = require('express-validator');
 const { v4: uuid } = require("uuid");
 
 let DUMMY_PLACES = [
@@ -65,6 +66,12 @@ const getPlacesByUserId = (req, res, next) => {
 };
 
 const createPlace = (req, res, next) => {
+  const errors = validationResult(req);
+  if(!errors.isEmpty())
+  {
+    console.log(errors);
+    throw new HttpError('Invalid inputs passed, please check your data',422);
+  }
   const { title, description, coordinates, address, creator } = req.body;
   const createdPlace = {
     id: uuid(),
--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -1,21 +1,27 @@
 const express = require("express");
+const { check } = require("express-validator");
 const router = express.Router();
 
-const placesControllers = require('../controllers/places-controllers.js');
-
+const placesControllers = require("../controllers/places-controllers.js");
 
 //ORDER MATTERS NICK
 
+router.get('/:pid', placesControllers.getPlaceById);
 
+router.get('/users/:uid', placesControllers.getPlacesByUserId);
 
-router.post('/',placesControllers.createPlace);
+router.post(
+  '/',
+  [
+    check('title').not().isEmpty(),
+    check('description').isLength({ min: 5, max: 256 }),
+    check('address').not().isEmpty(),
+  ],
+  placesControllers.createPlace
+);
 
-router.get("/:pid", placesControllers.getPlaceById);
+router.patch('/:pid', placesControllers.updatePlaceById);
 
-router.get("/users/:uid", placesControllers.getPlacesByUserId);
-
-router.patch('/:pid',placesControllers.updatePlaceById);
-
-router.delete('/:pid',placesControllers.deletePlaceById);
+router.delete('/:pid', placesControllers.deletePlaceById);
 
 module.exports = router;
