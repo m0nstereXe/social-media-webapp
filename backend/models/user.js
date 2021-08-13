6db224371c673c3905c924076693ef0362b655d7
Nick Belov <nickbelov@gmail.com>
2021-08-13 14:27:45 -0400

    users schema

---
+++ backend/models/user.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+const Schema = mongoose.Schema;
+
+//if u add unique to true it makes the look up faster because DB knows it can sort by that prop
+ const userSchema = new Schema({
+     name: {type: String, required: true},
+     email: {type: String, required: true, unique: true},
+     password: {type: String, required: true, minlength: 6},
+     image: {type: String, required: true},
+     places: {type: String, required: true}
+ });
+ userSchema.plugin(uniqueValidator); //this actually makes sure the email we send in is unique
+
+module.exports = mongoose.model('User',userSchema);
