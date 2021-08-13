4984eb15950034a7a11ba53ef44067c0600641a1
Nick Belov <nickbelov@gmail.com>
2021-08-13 15:05:03 -0400

    hooked up get users to db

--- backend/controllers/users-controllers.js
+++ backend/controllers/users-controllers.js
@@ -13,8 +13,22 @@ const DUMMY_USERS = [
   },
 ];
 
-const getUsers = (req, res, next) => {
-  res.status(200).json({ users: DUMMY_USERS });
+const getUsers = async (req, res, next) => {
+  //should prob use the cursor here instead but whatever
+  let users;
+  try {
+    //only asking for email and name and not password
+    users = await User.find({}, "name email");
+  } catch (error) {
+    return next(
+      new HttpError("Fetching users failed please try again later!!!", 500)
+    );
+  }
+  res.json({
+    users: users.map((u) => {
+      return u.toObject({ getters: true });
+    }),
+  });
 };
 const signup = async (req, res, next) => {
   const errors = validationResult(req);
@@ -62,9 +76,8 @@ const login = async (req, res, next) => {
     return next(new HttpError("Logging in Failed. Try again later!", 500));
   }
 
-  if(!existingUser || existingUser.password != password)
-  {
-    return next(new HttpError('Invalid Credentials!!!!',401));
+  if (!existingUser || existingUser.password != password) {
+    return next(new HttpError("Invalid Credentials!!!!", 401));
   }
 
   res.json({ message: "logged in!" });
