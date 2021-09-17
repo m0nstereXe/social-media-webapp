c8f03cfb9e50de11267d7413ecc2d00d0dcae7eb
Nick Belov <nickbelov@gmail.com>
2021-09-17 12:02:57 -0400

    see places via user

--- frontend/src/places/pages/UserPlaces.js
+++ frontend/src/places/pages/UserPlaces.js
@@ -1,44 +1,44 @@
-import React from 'react';
-import {useParams} from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
-import PlaceList from '../components/PlaceList';
+import PlaceList from "../components/PlaceList";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
+import { useHttpClient } from "../../shared/hooks/http-hook";
 
+const UserPlaces = () => {
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
+  const [loadedPlaces, setLoadedPlaces] = useState();
 
-const DUMMY_PLACES = [
-    {
-        id: 'p1',
-        title: 'Empire State Building',
-        description: 'big huge tower!!!!!',
-        imageUrl: 'https://images.emojiterra.com/twitter/v13.0/512px/1f9a7.png',
-        address: '376 Lakeview Drive, Wyckoff NJ',
-        location: 
-        {
-            lat: 40.9894912,
-            lng: -74.1605376,
-        },
-        creator: 'u1'
-    },
-    {
-        id: 'p2',
-        title: 'Poop',
-        description: 'look at this cool stuff!',
-        imageUrl: 'https://hips.hearstapps.com/edc.h-cdn.co/assets/16/45/1478626107-white-house-driveway.jpg',
-        address: '1600 Pensylvania Ave, Washington DC',
-        location: 
-        {
-            lat: 38.8977,
-            lng: -77.0365,
-        },
-        creator: 'u2'
-    },
-]
+  const userId = useParams().userId;
 
-const UserPlaces = () =>
-{
-    const userId = useParams().userId;
-    const loadedPlaces = DUMMY_PLACES.filter(place => place.creator === userId);
-    console.log(loadedPlaces);
-    return <PlaceList items={loadedPlaces}/>
+  //using effect to only render function once
+  useEffect(() => {
+    const fetchPlaces = async () => {
+      try {
+        const responseData = await sendRequest(
+          `http://localhost:5000/api/places/user/${userId}`
+        );
+        console.log(responseData.places);
+        setLoadedPlaces(responseData.places);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchPlaces();
+  }, [sendRequest, userId]);
+  console.log(loadedPlaces);
+  return (
+    <React.Fragment>
+      <ErrorModal error={error} onClear={clearError} />
+      {isLoading && (
+        <div className="center">
+          <LoadingSpinner />
+        </div>
+      )}
+      {!isLoading && loadedPlaces && <PlaceList items={loadedPlaces} />}
+    </React.Fragment>
+  );
 };
 
 export default UserPlaces;
