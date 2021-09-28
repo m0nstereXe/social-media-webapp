d1785ae5799a8f7e57509d7d1d4be47aff119a71
Nick Belov <nickbelov@gmail.com>
2021-09-28 16:14:07 -0400

    back end file validation

--- backend/routes/users-routes.js
+++ backend/routes/users-routes.js
@@ -3,15 +3,17 @@ const { check } = require("express-validator");
 const router = express.Router();
 
 const usersControllers = require("../controllers/users-controllers");
+const fileUpload = require("../middleware/file-upload");
 
 router.get("/", usersControllers.getUsers);
 
 router.post(
   "/signup",
+  fileUpload.single('image'),
   [
     check("name").not().isEmpty(),
     check("email").normalizeEmail().isEmail(),
-    check("password").isLength({min: 6}),
+    check("password").isLength({ min: 6 }),
   ],
   usersControllers.signup
 );
